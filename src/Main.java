613bc1b9ca17c1870c976ff5c7dadb23e4ba9f72
Andrey Petrov <rokanor@gmail.com>
2014-03-18 21:11:13 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrey
+ * Date: 2014-03-18
+ * Time: 8:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+
+
+        public static void main(String[] args) {
+
+            System.out.println("Test");
+
+
+
+        }
+
+}
