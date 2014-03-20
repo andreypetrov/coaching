68d493fe3bc22b3d2605f319dec91d220dfc463b
Andrey Petrov <rokanor@gmail.com>
2014-03-19 23:19:00 -0400

    triangle task edit 1

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,9 @@
 /**
- *
  * https://codility.com/train/
  * 1. Triangle
- *  Determine whether a triangle can be built from a given set of edges.
- *
- *
+ * Determine whether a triangle can be built from a given set of edges.
+ * <p/>
+ * <p/>
  * Created with IntelliJ IDEA.
  * User: andrey
  * Date: 2014-03-18
@@ -19,46 +18,40 @@ class Solution {
 
         //Important observation: if a..b..c are not consecutive triangle triplet then there is a triplet a'bc' that is consecutive and for it the triangular rule is true too
         sort(A);
-        for (int i = 0; i < A.length-2; i++) {
-            if (isTriange((long)A[i], (long)A[i + 1], (long)A[i + 2])) return 1;
+        for (int i = 0; i < A.length - 2; i++) {
+            if (isTriange(A[i], A[i + 1], A[i + 2])) return 1;
         }
-
         return 0;
     }
 
     public static boolean isTriange(long a, long b, long c) {
-        return (a+b>c && a+c > b && b+c > a);
+        return a + b > c && a + c > b && b + c > a;
     }
 
     public static void sort(int[] input) {
         if (input == null || input.length == 0) return;
-
-        int low = 0;
-        int high = input.length - 1;
-
-        quicksort(input, low, high);
+        quicksort(input, 0, input.length - 1);
     }
 
     public static void quicksort(int[] input, int low, int high) {
-        int medium = low + (high - low)/2;
-        int pivot = input[medium];
+        if (low >= high) return;
 
+        int medium = low + (high - low) / 2;
+        int pivot = input[medium];
         int i = low;
         int j = high;
 
-        while (i<=j) {
-            while(input[i] < pivot) i++;
-            while(input[j] > pivot) j--;
-
+        while (i <= j) {
+            while (input[i] < pivot) i++;
+            while (input[j] > pivot) j--;
             if (i<=j) {
-                swap(input, i,j);
+                swap(input, i, j);
                 i++;
                 j--;
             }
         }
-
-        if ( i < high)  quicksort(input, i, high);
-        if (j > low) quicksort(input, low, j);
+        quicksort(input, i, high);
+        quicksort(input, low, j);
     }
 
     private static void swap(int[] input, int i, int j) {
