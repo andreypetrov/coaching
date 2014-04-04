6edf9e13f950dfbb299254867726b2f0f8f3425e
Andrey Petrov <rokanor@gmail.com>
2014-04-04 00:09:51 -0400

    https://codility.com/train/ Lesson 7 – Maximum slice problem MaxDoubleSliceSum

---
+++ src/MaxDoubleSliceSum.java
@@ -0,0 +1,32 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrey
+ * Date: 2014-04-03
+ * Time: 11:25 PM
+ *
+ *
+ * Keep max slice minus the min element within that slice
+ */
+public class MaxDoubleSliceSum {
+
+    public int solution(int[] A) {
+        int maxSlice = 0;
+
+        int currentSlice = 0;
+        int currentMin = A[1];
+
+        for (int i = 2; i < A.length-1; i++) {
+            System.out.println("A[" + i + "]" + A[i]);
+            if(A[i] < currentMin){
+                currentSlice = currentSlice + currentMin;
+                currentMin = A[i];
+            } else {
+                currentSlice = currentSlice + A[i];
+            }
+            currentSlice = Math.max(0, currentSlice);
+            System.out.println(currentSlice);
+            maxSlice = Math.max(maxSlice, currentSlice);
+        }
+        return maxSlice;
+    }
+}
---
+++ tests/MaxDoubleSliceSumTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrey
+ * Date: 2014-04-03
+ * Time: 11:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MaxDoubleSliceSumTest {
+    @Test
+     public void testSolution() throws Exception {
+        MaxDoubleSliceSum sliceSum = new MaxDoubleSliceSum();
+
+            int[] A = {3, 2, 6, -1, 4, 5, -1, 2};
+        int result = sliceSum.solution(A);
+        assertEquals(17, result);
+
+
+    }
+
+    @Test
+    public void testSolutionSimple() throws Exception {
+        MaxDoubleSliceSum sliceSum = new MaxDoubleSliceSum();
+
+        int[] A = {3, -2, 6, 2};
+        int result = sliceSum.solution(A);
+        assertEquals(6, result);
+
+
+    }
+}
